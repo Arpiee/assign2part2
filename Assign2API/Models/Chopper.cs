e232db7b34015b67f3ccd3ae2bd4ae7f4ea04885
Arpiee <arpit.kohli01@gmail.com>
2018-04-11 21:49:31 -0400

    first

---
+++ Assign2API/Models/Chopper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assign2API.Models
+{
+    public class Chopper
+    {
+        [Key]
+        public int ChopperId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public decimal price { get; set; }
+    }
+}
---
+++ Assign2API/Models/Model1.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Assign2API.Models
+{
+    public class Model1 : DbContext
+    {
+        // onstructor
+        public Model1(DbContextOptions<Model1>options) : base(options)
+        {
+
+        }
+        public DbSet<Chopper> Choppers { get; set; }
+    }
+}
